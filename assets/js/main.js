5b98b769857b9f7c9b434b4b613b7ef0c134db7f
LucasjsSilva <luquinahjssilva@ufpi.edu.br>
2024-08-04 21:20:07 -0300

    manipulando o resultado de requisição da api atraves de uma promisse, utilizando JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-console.log("Funcionouu")
+
+const offset = 0;
+const limit = 20;
+const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
+fetch(url)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error))
+    .finally(() => console.log("Terminou"))
