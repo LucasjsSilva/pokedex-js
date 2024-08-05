4c469c66950d3619696b5a67d867739a502e8b7e
LucasjsSilva <luquinahjssilva@ufpi.edu.br>
2024-08-04 22:19:15 -0300

    separando informações

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,4 @@
 
-const offset = 0;
-const limit = 20;
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
-
 function convertPokemonToLi(pokemon){
     return ` 
             <li class="pokemon">
@@ -24,14 +20,10 @@ function convertPokemonToLi(pokemon){
 }
 
 const pokemonList = document.getElementById("pokemonlist")
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)         
-        }
-    })
-    .catch((error) => console.log(error))
-    .finally(() => console.log("Terminou"))
+
+pokeApi.getPokemons().then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        pokemonList.innerHTML += convertPokemonToLi(pokemon)         
+    }
+})
