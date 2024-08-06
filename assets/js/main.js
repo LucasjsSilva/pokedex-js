8062a6b848dcbffd3093918f2b6a007aa1393387
LucasjsSilva <luquinahjssilva@ufpi.edu.br>
2024-08-05 21:00:46 -0300

    convertendo o modelo da api para um modelo menos complexo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,14 @@
 
-function convertTypestoLi(pokemonsTypes){
-    return pokemonsTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
 function convertPokemonToLi(pokemon){
     return ` 
-            <li class="pokemon">
-                <span class="number">#${pokemon.order}</span>
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
                 <div class="detail">
                     <ol id="pokemonlist" class="types">
-                        ${convertTypestoLi(pokemon.types).join('')}
+                        ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                     </ol>
-                    <img src="${pokemon.sprites.other.dream_world.front_default}" alt=${pokemon.name}>
+                    <img src="${pokemon.photo}" alt=${pokemon.name}>
                 </div>
             </li> 
         `
