d0e320355f25b302cbbbbc66bb658391316daa84
LucasjsSilva <luquinahjssilva@ufpi.edu.br>
2024-08-07 22:46:04 -0300

    Criando mecanismo de paginação e limitando para a primeira geração

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,14 @@
 
-function convertPokemonToLi(pokemon){
-    return ` 
+const pokemonList = document.getElementById("pokemonlist")
+const loadmorebutton = document.getElementById("loadmorebutton")
+
+maxRecords = 151
+const limit = 10
+let offset = 0
+
+function loadmorepokemonitems(offset, limit) {    
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        pokemonList.innerHTML += newhtml = pokemons.map((pokemon) => ` 
             <li class="pokemon ${pokemon.type}">
                 <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
@@ -11,11 +19,21 @@ function convertPokemonToLi(pokemon){
                     <img src="${pokemon.photo}" alt=${pokemon.name}>
                 </div>
             </li> 
-        `
+        `).join('')
+        
+    })
 }
 
-const pokemonList = document.getElementById("pokemonlist")
+loadmorepokemonitems(offset, limit)
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += newList = pokemons.map(convertPokemonToLi).join('')
+loadmorebutton.addEventListener('click', () =>{
+    offset += limit
+    const qtRecornexPage = offset + limit
+    if (qtRecornexPage >= maxRecords){
+        const newLimit = maxRecords - offset
+        loadmorepokemonitems(offset, newLimit)
+        loadmorebutton.parentElement.removeChild(loadmorebutton)
+    }else{
+        loadmorepokemonitems(offset, limit)
+    }
 })
